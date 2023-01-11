927e25d9df63260d0f8cc2dd6cdee495a39f7f17
lovemelvm <nastyauskova1997@mail.ru>
2023-01-11 13:08:46 +0300

    Projects 005 Hello Masha

--- Projects005/Program.cs
+++ Projects005/Program.cs
@@ -1,7 +1,7 @@
 ﻿Console.Write("Введите имя пользователя: ");
 string username = Console.ReadLine();
 
-if (username == "Маша");
+if (username == "Маша")
 {
     Console.WriteLine("Ура, это же МАША!");
 }
